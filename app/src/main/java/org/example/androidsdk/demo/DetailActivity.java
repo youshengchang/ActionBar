486fa8501cf36e5ae1e2527e2b22a8961d43a46c
youshengchang <changyousheng@yahoo.com>
2015-02-27 16:01:38 -0800

    adding menu item using java in detail page

--- app/src/main/java/org/example/androidsdk/demo/DetailActivity.java
+++ app/src/main/java/org/example/androidsdk/demo/DetailActivity.java
@@ -1,7 +1,10 @@
 package org.example.androidsdk.demo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class DetailActivity extends Activity {
@@ -23,4 +26,22 @@ public class DetailActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item = menu.add(R.string.external_url);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String webpage = "http://developer.android.com/index.html";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+                startActivity(intent);
+
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
